8247a4e2b03b848f85f4fec546622ba78d2e5ec1
dwarjie <macoycoder@gmail.com>
2025-01-30 20:13:32 +0800

    add: part1 - statistics feat

--- part1/unicafe/src/Statistics.jsx
+++ part1/unicafe/src/Statistics.jsx
@@ -1,11 +1,24 @@
 const Statistics = ({ good, neutral, bad }) => {
-  return (
-    <>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </>
-  );
+	const getFeedbackNumber = () => {
+		return good + neutral + bad;
+	};
+
+	const getAverageFeedback = () => {
+		let badScore = bad * -1;
+		let totalScore = good + badScore;
+		console.log(badScore, totalScore);
+		return totalScore / getFeedbackNumber();
+	};
+
+	return (
+		<>
+			<p>good {good}</p>
+			<p>neutral {neutral}</p>
+			<p>bad {bad}</p>
+			<p>all {getFeedbackNumber()}</p>
+			<p>average {getAverageFeedback()}</p>
+		</>
+	);
 };
 
 export default Statistics;
