cf86d64dbc8808203f13f34c3bed17cc1f776c2d
dwarjie <macoycoder@gmail.com>
2025-02-01 14:58:56 +0800

    add: part2 - 2.9 exercise

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,45 +1,100 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const personInitialValue = [
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ];
 
-  const handleInputChange = (event) => {
-    setNewName(event.target.value)
-  }
+  const [persons, setPersons] = useState(personInitialValue);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [search, setSearch] = useState("");
+
+  const handleNewName = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNewNumber = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
 
   const addNote = (event) => {
-    event.preventDefault()
-    const personObj = prepareNoteValues()
-    setPersons(persons.concat(personObj))
-    setNewName("")
-  }
+    event.preventDefault();
+    if (checkDuplicate()) {
+      setNewName("");
+      setNewNumber("");
+      return alert(`${newName} is already added to phonebook.`);
+    }
+
+    const personObj = prepareNoteValues();
+    setPersons(persons.concat(personObj));
+    setNewName("");
+    setNewNumber("");
+  };
 
   const prepareNoteValues = () => {
     return {
       name: newName,
+      number: newNumber,
+    };
+  };
+
+  const checkDuplicate = () => {
+    const existingName = persons.find(({ name }) => name === newName);
+
+    return existingName;
+  };
+
+  const renderNumbers = () => {
+    if (!search) {
+      return persons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ));
     }
-  }
+
+    return persons
+      .filter((person) =>
+        person.name.toLowerCase().includes(search.toLowerCase()),
+      )
+      .map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))
+  };
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h2 style={{ fontWeight: "bold" }}>Phonebook</h2>
+      <div>
+        filter to shown with a
+        <input value={search} onChange={handleSearch} />
+      </div>
+      <h2 style={{ fontWeight: "bold" }}>add new</h2>
       <form onSubmit={addNote}>
         <div>
-          name: 
-          <input
-            value={newName}
-            onChange={handleInputChange}
-            />
+          name:
+          <input value={newName} onChange={handleNewName} />
+        </div>
+        <div>
+          number:
+          <input value={newNumber} onChange={handleNewNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
-      {persons.map((person) => (
-        <p key={person.name}>{person.name}</p>
-      ))}
+      <h2 style={{ fontWeight: "bold" }}>Numbers</h2>
+      {renderNumbers()}
     </div>
   );
 };
