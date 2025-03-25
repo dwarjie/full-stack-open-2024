841306ba0c9a7ac34fdc2f4842ea574e9d23e1de
dwarjie <macoycoder@gmail.com>
2025-03-25 21:31:18 +0800

    add: part4 - 4.2

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,46 +1,24 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-console.log("sample eslint");
+const logger = require("./utils/logger");
+const config = require("./utils/config");
+const blogRouter = require("./controllers/blog");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+logger.info("Connecting to database ...");
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(config.MONGO_URI)
   .then(() => {
-    console.log("Connected to database");
+    logger.info("Connected to database!");
   })
   .catch((err) => {
-    console.error(`Error connecting to database: ${err}`);
+    logger.error(`Error connecting to database: ${err}`);
   });
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/blogs", blogRouter);
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = process.env.PORT || 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+module.exports = app;
